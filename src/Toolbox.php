6afa2436b13887f04b8998a3a5c4a314673ee7a1
Seth Battis <sethbattis@stmarksschool.org>
2017-03-27 08:50:20 -0400

    PSR-2 tidying

--- src/Toolbox.php
+++ src/Toolbox.php
@@ -33,4 +33,32 @@ class Toolbox extends \smtech\StMarksReflexiveCanvasLTI\Toolbox
 
         return $this->generator;
     }
+
+    /**
+     * This is a quasi temporary "unborking" while a few support tickets are
+     * working through the queue. See the fix-me notations below.
+     *
+     * @param string $previewUrl
+     * @return string
+     */
+    public function unborkPreviewUrl($previewUrl)
+    {
+        if (!preg_match('%^https?://.*%', $previewUrl)) {
+            /*
+             * FIXME: per [case 01584858 ](https://cases.canvaslms.com/CommunityConsole?id=500A000000UPwauIAD)
+             * attachments are not well-documented and the Crocodoc attachments
+             * include an incomplete preview URL that works… but not in an IFRAME
+             */
+            return $_SESSION[CANVAS_INSTANCE_URL] . $previewUrl;
+        } elseif (preg_match('%^(.*version=)(\d+)(.*)$%', $previewUrl, $match)) {
+            /*
+             * FIXME: per [case 01584819](https://cases.canvaslms.com/CommunityConsole?id=500A000000UPwSlIAL)
+             * preview URLs are generated "off by one" and are really zero-indexed.
+             */
+            return $match[1] . ($match[2] - 1) . $match[3];
+        } else {
+            /* whatevs, it is what it is… */
+            return $previewUrl;
+        }
+    }
 }
--- submissions.php
+++ submissions.php
@@ -5,31 +5,6 @@ require_once 'common.inc.php';
 use smtech\ReflexiveCanvasLTI\LTI\ToolProvider;
 use Battis\DataUtilities;
 
-/**
- * This is a quasi temporary "unborking" while a few support tickets are
- * working through the queue. See the fix-me notations below.
- *
- * @param string $previewUrl
- * @return string
- */
-function unborkPreviewUrl($previewUrl)
-{
-    if (!preg_match('%^https?://.*%', $previewUrl)) {
-        /*
-         * FIXME: per [case 01584858 ](https://cases.canvaslms.com/CommunityConsole?id=500A000000UPwauIAD) attachments are not well-documented and the Crocodoc attachments include an incomplete preview URL that works… but not in an IFRAME
-         */
-        return $_SESSION[CANVAS_INSTANCE_URL] . $previewUrl;
-    } elseif (preg_match('%^(.*version=)(\d+)(.*)$%', $previewUrl, $match)) {
-        /*
-         * FIXME: per [case 01584819](https://cases.canvaslms.com/CommunityConsole?id=500A000000UPwSlIAL) preview URLs are generated "off by one" and are really zero-indexed.
-         */
-        return $match[1] . ($match[2] - 1) . $match[3];
-    } else {
-        /* whatevs, it is what it is… */
-        return $previewUrl;
-    }
-}
-
 $enrollments = $toolbox->api_get(
     'courses/' . $_SESSION[ToolProvider::class]['canvas']['course_id'] . '/enrollments',
     [
@@ -60,11 +35,12 @@ if ($isStudent) {
 
     foreach ($submissions as $submission) {
         $assignment = $toolbox->api_get(
-            'courses/' . $_SESSION[ToolProvider::class]['canvas']['course_id'] . "/assignments/{$submission['assignment_id']}"
+            'courses/' . $_SESSION[ToolProvider::class]['canvas']['course_id'] .
+            "/assignments/{$submission['assignment_id']}"
         );
         if (!in_array('not_graded', $assignment['submission_types'])) {
             $assignmentData['assignment'] = $assignment;
-            foreach($submission['submission_history'] as $version) {
+            foreach ($submission['submission_history'] as $version) {
                 if (!empty($version['submitted_at'])) {
                     $versionData = [
                         'id' => $version['id'],
@@ -75,18 +51,26 @@ if ($isStudent) {
                         $versionData['body'] = $version['body'];
                     } else {
                         if (empty($version['attachments'])) {
-                            $versionData['type'] = DataUtilities::titleCase(str_replace('_', ' ', $version['submission_type']));
-                            $versionData['preview_url'] = unborkPreviewUrl($version['preview_url']);
+                            $versionData['type'] = DataUtilities::titleCase(
+                                str_replace(
+                                    '_',
+                                    ' ',
+                                    $version['submission_type']
+                                )
+                            );
+                            $versionData['preview_url'] = $toolbox->unborkPreviewUrl($version['preview_url']);
                         } else {
                             foreach ($version['attachments'] as $attachment) {
                                 $versionData['attachments'][$attachment['id']] = [
                                         'name' => $attachment['display_name'],
-                                        'preview_url' => unborkPreviewUrl($attachment['preview_url'])
+                                        'preview_url' => $toolbox->unborkPreviewUrl($attachment['preview_url'])
                                     ];
                             }
                         }
                     }
-                    if (!empty($versionData['body']) || !empty($versionData['preview_url']) || !empty($versionData['attachments'])) {
+                    if (!empty($versionData['body']) ||
+                        !empty($versionData['preview_url']) ||
+                        !empty($versionData['attachments'])) {
                         $assignmentData['submissions'][$versionData['attempt']] = $versionData;
                     }
                 }
